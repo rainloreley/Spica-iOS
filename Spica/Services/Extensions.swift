9768b397f7d665c3b335b8f3837e31e1f37beb5a
Adrian Baumgart <adrian@abmgrt.dev>
2020-07-04 21:40:05 +0200

    added buttons to post create, added photo uploading and added better error messages
    Signed-off-by: Adrian Baumgart <adrian@abmgrt.dev>

--- Spica/Services/Extensions.swift
+++ Spica/Services/Extensions.swift
@@ -64,3 +64,10 @@ extension UIViewController {
         view.endEditing(true)
     }
 }
+
+extension UIImage {
+	func toBase64() -> String? {
+		guard let imageData = self.pngData() else { return nil }
+		return imageData.base64EncodedString(options: Data.Base64EncodingOptions.lineLength64Characters)
+	}
+}
--- Spica/View Controllers/PostCreateViewController.swift
+++ Spica/View Controllers/PostCreateViewController.swift
@@ -8,17 +8,22 @@
 import KMPlaceholderTextView
 import SnapKit
 import UIKit
+import SwiftKeychainWrapper
 
 protocol PostCreateDelegate {
     func didSendPost(sentPost: SentPost)
 }
 
 class PostCreateViewController: UIViewController {
-    var sendButton: UIButton!
+    //var sendButton: UIButton!
     var userPfp: UIImageView!
     var contentTextView: KMPlaceholderTextView!
     var type: PostType!
     var parentID: String?
+	var imageButton: UIBarButtonItem!
+	
+	var selectedImage: UIImage?
+	var imagePicker: UIImagePickerController!
 
     var delegate: PostCreateDelegate!
 
@@ -28,17 +33,36 @@ class PostCreateViewController: UIViewController {
         hideKeyboardWhenTappedAround()
         navigationItem.title = type == PostType.post ? "Post" : "Reply"
         navigationController?.navigationBar.prefersLargeTitles = false
+		
+		imagePicker = UIImagePickerController()
+		imagePicker.allowsEditing = false
+		imagePicker.mediaTypes = ["public.image"]
+		imagePicker.delegate = self
+		
+		imageButton = UIBarButtonItem(image: UIImage(systemName: "photo"), style: .plain, target: self, action: #selector(self.openImagePicker))
+		
+		let sendButton = UIBarButtonItem(image: UIImage(systemName: "paperplane.fill"), style: .plain, target: self, action: #selector(self.sendPost))
+		
+		navigationItem.leftBarButtonItem = imageButton
+		navigationItem.rightBarButtonItem = sendButton
 
-        sendButton = UIButton(type: .system)
+        /*sendButton = UIButton(type: .system)
         sendButton.setTitle(type == PostType.post ? "Post" : "Reply", for: .normal)
         sendButton.setTitleColor(.white, for: .normal)
         sendButton.backgroundColor = UIColor(named: "PostButtonColor")
         sendButton.layer.cornerRadius = 12
         sendButton.titleLabel?.font = .boldSystemFont(ofSize: 17)
         sendButton.addTarget(self, action: #selector(sendPost), for: .touchUpInside)
-        view.addSubview(sendButton)
+        view.addSubview(sendButton)*/
+		
+		/*imageButton = UIButton(type: .system)
+		imageButton.setImage(UIImage(systemName: "photo"), for: .normal)
+		imageButton.addTarget(self, action: #selector(self.openImagePicker), for: .touchUpInside)
+		view.addSubview(imageButton)*/
+		
+		let userUsername = KeychainWrapper.standard.string(forKey: "dev.abmgrt.spica.user.username")
 
-        let pfpImage = ImageLoader.default.loadImageFromInternet(url: URL(string: "https://avatar.alles.cx/u/adrian")!)
+        let pfpImage = ImageLoader.default.loadImageFromInternet(url: URL(string: "https://avatar.alles.cx/u/\(userUsername!)")!)
         userPfp = UIImageView(frame: .zero)
         userPfp.image = pfpImage
         userPfp.layer.cornerRadius = 20
@@ -49,15 +73,16 @@ class PostCreateViewController: UIViewController {
         contentTextView = KMPlaceholderTextView(frame: .zero)
         contentTextView.font = .systemFont(ofSize: 18)
         contentTextView.placeholder = "What's on your mind?"
+		contentTextView.placeholderColor = UIColor.tertiaryLabel
 
         view.addSubview(contentTextView)
 
-        sendButton.snp.makeConstraints { make in
+        /*sendButton.snp.makeConstraints { make in
             make.bottom.equalTo(view.snp.bottom).offset(-50)
             make.centerX.equalTo(view.snp.centerX)
             make.height.equalTo(50)
             make.width.equalTo(view.snp.width).offset(-32)
-        }
+        }*/
 
         userPfp.snp.makeConstraints { make in
             make.height.equalTo(40)
@@ -65,16 +90,41 @@ class PostCreateViewController: UIViewController {
             make.top.equalTo(view.snp.top).offset(80)
             make.left.equalTo(view.snp.left).offset(16)
         }
+		
+		/*imageButton.snp.makeConstraints { (make) in
+			make.height.equalTo(40)
+			make.width.equalTo(40)
+			make.bottom.equalTo(view.snp.bottom).offset(-80)
+			make.left.equalTo(view.snp.left).offset(16)
+		}*/
 
         contentTextView.snp.makeConstraints { make in
             make.top.equalTo(view.snp.top).offset(80)
             make.left.equalTo(view.snp.left).offset(72)
             make.right.equalTo(view.snp.right).offset(-32)
-            make.bottom.equalTo(sendButton.snp.top).offset(-16)
+			make.bottom.equalTo(view.snp.bottom).offset(-16)
         }
 
         // Do any additional setup after loading the view.
     }
+	
+	@objc func openImagePicker(sender: UIBarButtonItem) {
+		print(selectedImage)
+		if selectedImage != nil {
+			EZAlertController.actionSheet("Image", message: "Select an action", sourceView: view, actions: [
+			UIAlertAction(title: "Select another image", style: .default, handler: { (_) in
+				//self.imagePicker.present(from: self.view)
+				self.present(self.imagePicker, animated: true, completion: nil)
+			}),UIAlertAction(title: "Remove", style: .destructive, handler: { (_) in
+				self.selectedImage = nil
+				self.imageButton.image = UIImage(systemName: "photo")
+			}), UIAlertAction(title: "Cancel", style: .cancel, handler: nil)])
+		}
+		else {
+			present(imagePicker, animated: true, completion: nil)
+		}
+		
+	}
 
     @objc func sendPost() {
         contentTextView.layer.cornerRadius = 0
@@ -86,7 +136,7 @@ class PostCreateViewController: UIViewController {
             contentTextView.layer.borderColor = UIColor.systemRed.cgColor
             return
         } else {
-            AllesAPI.default.sendPost(newPost: NewPost(content: contentTextView.text, image: nil, type: type, parent: parentID)) { result in
+            AllesAPI.default.sendPost(newPost: NewPost(content: contentTextView.text, image: selectedImage, type: type, parent: parentID)) { result in
                 switch result {
                 case let .success(sentPost):
 
@@ -122,6 +172,22 @@ class PostCreateViewController: UIViewController {
          // Pass the selected object to the new view controller.
      }
      */
+}
+
+extension PostCreateViewController: UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+	func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+		   if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+			self.selectedImage = pickedImage
+			imageButton.image = UIImage(systemName: "photo.fill")
+		   }
+		   
+		   dismiss(animated: true, completion: nil)
+	   }
+	   
+	   func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+		   dismiss(animated: true, completion: nil)
+	   }
+	
 }
 
 enum PostType {
