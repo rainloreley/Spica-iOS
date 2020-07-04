875a113812c7aa61c03ba0274cd7e503ba691742
Adrian Baumgart <adrian@abmgrt.dev>
2020-07-04 21:40:46 +0200

    useless extension
    Signed-off-by: Adrian Baumgart <adrian@abmgrt.dev>

--- Spica/Services/Extensions.swift
+++ Spica/Services/Extensions.swift
@@ -64,10 +64,3 @@ extension UIViewController {
         view.endEditing(true)
     }
 }
-
-extension UIImage {
-	func toBase64() -> String? {
-		guard let imageData = self.pngData() else { return nil }
-		return imageData.base64EncodedString(options: Data.Base64EncodingOptions.lineLength64Characters)
-	}
-}
