cac97dc45a30cb87dbe4b8c27b6e82718f776a8b
Adrian Baumgart <adrian@abmgrt.dev>
2020-07-11 18:16:21 +0200

    added cache (and fixed a bug)
    Signed-off-by: Adrian Baumgart <adrian@abmgrt.dev>

--- Spica/Services/ImageLoader.swift
+++ Spica/Services/ImageLoader.swift
@@ -8,16 +8,32 @@
 import Foundation
 import UIKit
 
+let imageCache = NSCache<NSString, UIImage>()
+
 public class ImageLoader {
     static let `default` = ImageLoader()
     public func loadImageFromInternet(url: URL) -> UIImage {
-        let tempImg: UIImage?
-        let data = try? Data(contentsOf: url)
-        if data != nil {
-            tempImg = UIImage(data: data!)
-        } else {
-            tempImg = UIImage(systemName: "person")
-        }
-        return tempImg!
+		
+		if let cachedImage = imageCache.object(forKey: url.absoluteString as NSString) {
+			print("Cached: \(url.absoluteString)")
+			return cachedImage
+		}
+		else {
+			
+			print("Not Cached: \(url.absoluteString)")
+			let tempImg: UIImage?
+			let data = try? Data(contentsOf: url)
+			if data != nil {
+				tempImg = UIImage(data: data!)
+				imageCache.setObject(tempImg!, forKey: url.absoluteString as NSString)
+				print("Now Cached: \(url.absoluteString)")
+			} else {
+				tempImg = UIImage(systemName: "person")
+			}
+			
+			return tempImg!
+		}
+		
+        
     }
 }
