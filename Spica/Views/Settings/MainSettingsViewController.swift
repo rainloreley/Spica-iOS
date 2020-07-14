72b061d6ef9958c9db6a5558baf1c67ce93b1b83
Adrian Baumgart <adrian@abmgrt.dev>
2020-07-14 15:43:42 +0200

    added privacy policy
    Signed-off-by: Adrian Baumgart <adrian@abmgrt.dev>

--- Spica/Views/Settings/MainSettingsViewController.swift
+++ Spica/Views/Settings/MainSettingsViewController.swift
@@ -35,7 +35,21 @@ class MainSettingsViewController: UITableViewController {
         mySceneDelegate.window?.makeKeyAndVisible()
     }
 
-    @IBAction func allesPrivacy(_: Any) {
+	@IBAction func spicaPrivacy(_ sender: Any) {
+		let url = URL(string: "https://spica.fliney.eu/privacy")
+		if UIApplication.shared.canOpenURL(url!) {
+			UIApplication.shared.open(url!)
+		}
+	}
+	
+	@IBAction func spicaWebsite(_ sender: Any) {
+		let url = URL(string: "https://spica.fliney.eu/")
+		if UIApplication.shared.canOpenURL(url!) {
+			UIApplication.shared.open(url!)
+		}
+	}
+	
+	@IBAction func allesPrivacy(_: Any) {
         let url = URL(string: "https://alles.cx/docs/privacy")
         if UIApplication.shared.canOpenURL(url!) {
             UIApplication.shared.open(url!)
@@ -48,7 +62,14 @@ class MainSettingsViewController: UITableViewController {
             UIApplication.shared.open(url!)
         }
     }
-
+	
+	@IBAction func allesWebsite(_ sender: Any) {
+		let url = URL(string: "https://alles.cx/")
+		if UIApplication.shared.canOpenURL(url!) {
+			UIApplication.shared.open(url!)
+		}
+	}
+	
     @IBAction func usedLibraries(_: Any) {
         navigationController?.pushViewController(UsedLibrariesViewController(), animated: true)
     }
@@ -104,7 +125,7 @@ class MainSettingsViewController: UITableViewController {
         case 1:
             return 2
         case 2:
-            return 2
+			return 3
         case 3:
             return 2
         case 4:
@@ -113,59 +134,4 @@ class MainSettingsViewController: UITableViewController {
             return 0
         }
     }
-
-    /*
-     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-         // Configure the cell...
-
-         return cell
-     }
-     */
-
-    /*
-     // Override to support conditional editing of the table view.
-     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-         // Return false if you do not want the specified item to be editable.
-         return true
-     }
-     */
-
-    /*
-     // Override to support editing the table view.
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-         if editingStyle == .delete {
-             // Delete the row from the data source
-             tableView.deleteRows(at: [indexPath], with: .fade)
-         } else if editingStyle == .insert {
-             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-         }
-     }
-     */
-
-    /*
-     // Override to support rearranging the table view.
-     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-     }
-     */
-
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-         // Return false if you do not want the item to be re-orderable.
-         return true
-     }
-     */
-
-    /*
-     // MARK: - Navigation
-
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-         // Get the new view controller using segue.destination.
-         // Pass the selected object to the new view controller.
-     }
-     */
 }
